2b85bd4c974b36b380fbe462d1520eee0a0e82e9
jehosaphat94 <49460067+jehosaphat94@users.noreply.github.com>
2021-05-11 13:50:57 -0500

    Add files via upload

---
+++ Meraki VPN.ps1
@@ -0,0 +1,6 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy AllSigned -Scope Process
+
+$ServerAddress = "vpn.sumnercountytn.gov"
+$ConnectionName = "Sumner Co VPN"
+$PresharedKey = "Sumn3rC0unty1!"
+Add-VpnConnection -Name "$ConnectionName" -ServerAddress "$ServerAddress" -TunnelType L2tp -AllUserConnection -L2tpPsk "$PresharedKey" -AuthenticationMethod Pap -Force
---
+++ User info Copy.ps1
@@ -0,0 +1,23 @@
+﻿#THIS COMMAND ALLOWS THE SCRIPT TO RUN
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine;
+#CHANGE USER FOR WHICHEVER ACCOUNT NEEDS TO BE COPIED
+$User = "\Tdean"
+$Documents = "\Documents*"
+$Downloads = "\Downloads*"
+$Pictures = "\Pictures*"
+$Desktop = "\Desktop*"
+$Path = "C:\Users*"
+$PartDoc= join-path -path $User -childpath $Documents
+$PartDow= join-path -path $User -childpath $Downloads
+$PartPic= join-path -path $User -childpath $Pictures
+$PartDes= join-path -path $User -childpath $Desktop
+$FullDoc= join-path -path $Path -childpath $PartDoc
+$FullDow= join-path -path $Path -childpath $PartDow
+$FullPic= join-path -path $Path -childpath $PartPic
+$FullDes= join-path -path $Path -childpath $PartDes
+Copy-Item -Path $FullDoc -Destination D: -recurse -Force;
+Copy-Item -Path $FullDow -Destination D: -recurse -Force;
+Copy-Item -Path $FullPic -Destination D: -recurse -Force;
+Copy-Item -Path $FullDes -Destination D: -recurse -Force;
+#THIS SETS THE SCRIPT PERMISSIONS BACK TO RESTRICTED
+Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope LocalMachine
